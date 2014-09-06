45e647c621a6686c7598f0475a202c11d914a681
Matt Cox <mattcoxpdx@gmail.com>
2014-09-06 04:18:01 -0700

    Program displays splash upon start

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func splashScreen(args []string) {
+	fmt.Println(" ====== ")
+	fmt.Println("| gonc |")
+	fmt.Println(" ====== ")
+
+	for _, arg := range args {
+		fmt.Printf(" %s,", arg)
+	}
+
+	fmt.Println("\n Started: %s", time.Now())
+}
+
+func main() {
+	args := os.Args[1:]
+	splashScreen(args)
+}
